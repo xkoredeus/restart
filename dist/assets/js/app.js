e9fdf56931d0cb47e4b691f372addf3115bb4c36
Сергей Начаров <nacharovsa@it2g.ru>
2022-03-25 00:49:39 +0400

    fixes

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -116,8 +116,54 @@ $(() => {
 });
 
 $(() => {
+  $.fn.select2.amd.define('select2/i18n/ru',[],function () {
+    // Russian
+    return {
+      errorLoading: function () {
+        return 'Результат не может быть загружен.';
+      },
+      inputTooLong: function (args) {
+        var overChars = args.input.length - args.maximum;
+        var message = 'Пожалуйста, удалите ' + overChars + ' символ';
+        if (overChars >= 2 && overChars <= 4) {
+          message += 'а';
+        } else if (overChars >= 5) {
+          message += 'ов';
+        }
+        return message;
+      },
+      inputTooShort: function (args) {
+        var remainingChars = args.minimum - args.input.length;
+
+        var message = 'Пожалуйста, введите ' + remainingChars + ' или более символов';
+
+        return message;
+      },
+      loadingMore: function () {
+        return 'Загружаем ещё ресурсы…';
+      },
+      maximumSelected: function (args) {
+        var message = 'Вы можете выбрать ' + args.maximum + ' элемент';
+
+        if (args.maximum  >= 2 && args.maximum <= 4) {
+          message += 'а';
+        } else if (args.maximum >= 5) {
+          message += 'ов';
+        }
+
+        return message;
+      },
+      noResults: function () {
+        return 'Ничего не найдено';
+      },
+      searching: function () {
+        return 'Поиск…';
+      }
+    };
+  });
   $(".js-company-select").select2({
-    placeholder: "Название компании"
+    placeholder: 'Название компании',
+    language: 'ru'
   });
   $(".js-common-select").select2({
     minimumResultsForSearch: -1,
